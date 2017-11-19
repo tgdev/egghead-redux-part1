d4f2c091b231a3b9f4735f2341e6ca18415fa686
Tom Gillard <tomgillar@gmail.com>
2017-11-19 16:47:28 +1100

    Replaced custom Provider with react-redux

--- src/index.js
+++ src/index.js
@@ -3,23 +3,24 @@ import 'babel-polyfill';
 import React from 'react';
 import ReactDOM from 'react-dom';
 import PropTypes from 'prop-types';
+import { Provider } from 'react-redux';
 
 import appStore from './store';
 
 import TodoApp from './components/App';
 
 // temp provider from video 25
-class Provider extends React.Component {
-  getChildContext() {
-    return {
-      store: this.props.store
-    };
-  }
-
-  render() {
-    return this.props.children;
-  }
-}
+// class Provider extends React.Component {
+//   getChildContext() {
+//     return {
+//       store: this.props.store
+//     };
+//   }
+//
+//   render() {
+//     return this.props.children;
+//   }
+// }
 
 Provider.propTypes = {
   store: PropTypes.object.isRequired,
