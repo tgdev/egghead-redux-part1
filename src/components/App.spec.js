58580688737f659a7f86c4ec95c1d86911b6f2c9
Tom Gillard <tgillard@seek.com.au>
2017-11-20 10:56:00 +1100

    Updated App test after refactor

--- src/components/App.spec.js
+++ src/components/App.spec.js
@@ -1,28 +1,13 @@
 import React from 'react';
 import { shallow } from 'enzyme';
 
-import { FILTER_OPTION_ALL } from 'constants/visibility-filter-options';
-
-import newTodosMock from 'mocks/new-todos.mock';
-
 import App from './App';
 
 describe('App component', () => {
 
-  const mockTodos = newTodosMock;
-
-  const mockVisibilityFilter = FILTER_OPTION_ALL;
-
-  it('renders TodoApp correctly with no todos', () => {
-    const wrapper = shallow(<App todos={[]} visibilityFilter={mockVisibilityFilter} />);
+  it('renders TodoApp correctly', () => {
+    const wrapper = shallow(<App />);
     expect(wrapper).toMatchSnapshot();
-    // expect(wrapper.instance().props.todos.length).toEqual(0);
-  });
-
-  it('renders TodoApp correctly with saved todos', () => {
-    const wrapper = shallow(<App todos={mockTodos} visibilityFilter={mockVisibilityFilter} />);
-    expect(wrapper).toMatchSnapshot();
-    // expect(wrapper.instance().props.todos.length).toEqual(2);
   });
 
 });
